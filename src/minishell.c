8ae314e786c8b8e5e26dbdaff4857edd82b37c8c
Youness Lagmah <ylagmah@e1r3p1.1337.ma>
2025-02-13 16:22:31 +0100

    Fix seg on ctr + d

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ylagmah <ylagmah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 11:35:32 by ylagmah           #+#    #+#             */
-/*   Updated: 2025/02/13 14:48:40 by ylagmah          ###   ########.fr       */
+/*   Updated: 2025/02/13 16:21:44 by ylagmah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,9 @@ static void	minishell(t_shell *shell)
 	while (!shell->exit)
 	{
 		str = readline("\033[32mminishell >> \033[0m");
+		if (!str)
+			break ;
 		ft_add_garbage(&(shell->cmd_garbage), shell, str);
-
 		cmd = ft_parser(shell, str);
 		if (cmd)
 			ft_execute(shell, cmd->node);
@@ -30,15 +31,10 @@ static void	minishell(t_shell *shell)
 	ft_clean_all(shell);
 }
 
-void f()
-{
-	system("leaks minishell");
-}
-
 int	main(int argc, char **argv, char **env)
 {
 	t_shell	shell;
-	atexit(f);
+
 	(void) argv;
 	if (argc != 1)
 		return (ft_perror("usage => ./minishell"), 1);
