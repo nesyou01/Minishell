b7154452ceaba2452fbdfe088e4bb75a11a1b3a3
Youness Lagmah <ylagmah@e3r7p2.1337.ma>
2025-02-16 13:56:46 +0100

    Fix vars expanding 3

--- src/parser/expander.c
+++ src/parser/expander.c
@@ -6,7 +6,7 @@
 /*   By: ylagmah <ylagmah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 16:27:00 by ylagmah           #+#    #+#             */
-/*   Updated: 2025/02/16 13:38:07 by ylagmah          ###   ########.fr       */
+/*   Updated: 2025/02/16 13:56:03 by ylagmah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,12 @@ static int	expand_var(t_shell *shell, t_node *node, int start)
 	char	*start_str;
 	char	*end_str;
 
-	if (start > 100)
-		return (1);
 	if (!start)
 		start_str = NULL;
 	else
 		start_str = ft_substr(shell, node->content, 0, start);
 	end = get_var_end(node->content + start) + start + 1;
-	var = ft_substr(shell, node->content, start, end);
+	var = ft_substr(shell, node->content, start, end - start);
 	expanded = ft_expand_all_vars(shell, var);
 	end_str = ft_substr(shell, node->content, end, ft_strlen(node->content) - end);
 	node->content = ft_strjoin(shell, ft_strjoin(shell, start_str, expanded), end_str);
