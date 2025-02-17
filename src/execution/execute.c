b021f59e9629411c6bda529e18fc6ce10613c001
Youness Lagmah <ylagmah@e2r4p10.1337.ma>
2025-02-17 11:32:53 +0100

    Widlcard part 2

--- src/execution/execute.c
+++ src/execution/execute.c
@@ -6,7 +6,7 @@
 /*   By: ylagmah <ylagmah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 16:28:05 by ylagmah           #+#    #+#             */
-/*   Updated: 2025/02/17 10:30:10 by ylagmah          ###   ########.fr       */
+/*   Updated: 2025/02/17 11:09:03 by ylagmah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	ft_execute(t_shell *shell, t_node *node)
 {
-	(void) shell;
 	if (!node)
 		return ;
 	// TODO
 	while (node)
 	{
-		ft_expand_node_vars(shell, node);
+		if (ft_expand_node_vars(shell, node))
+			return ;
 		printf("--> %s -- %d\n", node->content, node->type);
 		node = node->next;
 	}
--- src/parser/expand_wildcards.c
+++ src/parser/expand_wildcards.c
@@ -6,13 +6,32 @@
 /*   By: ylagmah <ylagmah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 10:33:36 by ylagmah           #+#    #+#             */
-/*   Updated: 2025/02/17 11:03:54 by ylagmah          ###   ########.fr       */
+/*   Updated: 2025/02/17 11:32:27 by ylagmah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static char	*read_dir(t_shell *shell, DIR *dir)
+static char	*sub_str_until(t_shell *shell, char *str, char c)
+{
+	int	until;
+
+	until = 0;
+	while (str[until] && until != c)
+		until++;
+	if (!until)
+		return (NULL);
+	return (ft_substr(shell, str, 0, until + 1));
+}
+
+static int	ft_matches(char *str, char *filter)
+{
+	if (!filter)
+		return (1);
+	return (ft_strnstr(str, filter, ft_strlen(str)) != NULL);
+}
+
+static char	*read_dir(t_shell *shell, DIR *dir, char *filter)
 {
 	char			*str;
  	struct dirent	*dr;
@@ -23,7 +42,7 @@ static char	*read_dir(t_shell *shell, DIR *dir)
 		dr = readdir(dir);
 		if (!dr)
 			break ;
-		if (dr->d_name[0] != '.')
+		if (dr->d_name[0] != '.' && ft_matches(dr->d_name, filter))
 		{
 			if (str)
 				str = ft_strjoin(shell, str, " ");
@@ -36,11 +55,25 @@ static char	*read_dir(t_shell *shell, DIR *dir)
 int	ft_expand_wildcards(t_shell *shell, t_node *node, int start)
 {
 	DIR		*dir;
-
+	char	*filter;
+	char	*expanded;
+	char	*start_str;
+	char	*end;
+	
 	dir = opendir(ft_get_env_value(shell, "PWD"));
  	if (!dir)
 		return (ft_perror("Cannot open dir!"), -1);
-	printf("%s\n", read_dir(shell, dir));
+	filter = sub_str_until(shell, node->content + start + 1, ' ');
+	expanded = read_dir(shell, dir, filter);
 	closedir(dir);
-	return (1);
+	if (!expanded)
+		return (ft_safe_strlen(filter) + 1);
+	if (!start)
+		start_str = NULL;
+	else
+		start_str = ft_substr(shell, node->content, 0, start);
+	end = node->content + start + ft_safe_strlen(filter) + 1;
+	node->content = ft_strjoin(shell,
+			ft_strjoin(shell, start_str, expanded), end);
+	return (ft_safe_strlen(expanded));
 }
