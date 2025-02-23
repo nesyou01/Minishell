8ccd72d7b88470f9541ea971a0141a8e12318118
Youness Lagmah <ylagmah@e3r7p14.1337.ma>
2025-02-23 08:56:16 +0000

    Split header files

---
+++ includes/defs.h
@@ -0,0 +1,82 @@
+#ifndef DEFS_H
+# define DEFS_H
+# include "../libs/libft/libft.h"
+
+# define SEPECIAL_CHARS "<>|&() "
+# define UNKNOWN -1
+# define OUT_REDIRECTER 1
+# define IN_REDIRECTER 2
+# define HERE_DOC 3
+# define OUT_APPEND_REDIRECTER 4
+# define PIPE 105
+# define STATUS_CODE 6
+# define FILE 7
+# define COMMAND 9
+# define ARGUMENT 10
+# define HERE_DOC_LIMITER 12
+# define PARENTHESES_START 113
+# define PARENTHESES_END 114
+# define AND 115
+# define OR 116
+
+typedef struct s_token
+{
+	int				type;
+	char			*content;
+	struct s_token	*next;
+	struct s_token	*prev;
+}	t_token;
+
+typedef struct s_file
+{
+	int				fd;
+	char			*path;
+	struct s_file	*next;
+}	t_file;
+
+typedef struct s_node
+{
+	int				type;
+	int				taken;
+	char			*content;
+	char			*filter;
+	t_file			*here_doc;
+	t_file			*in;
+	t_file			*out;
+	struct s_node	*next;
+	struct s_node	*prev;
+	struct s_node	*r_node;
+	struct s_node	*l_node;
+}	t_node;
+
+typedef struct s_env
+{
+	char			*key;
+	char			*value;
+	struct s_env	*next;
+}	t_env;
+
+typedef struct s_shell
+{
+	int		exit;
+	t_env	*env;
+	t_list	*globale_garbage;
+	t_list	*cmd_garbage;
+}	t_shell;
+
+typedef struct s_execute
+{
+	t_file			*in;
+	t_file			*out;
+	t_file			*here_doc;
+	t_node			*node;
+}	t_execute;
+
+typedef struct s_command
+{
+	t_list	*argv;
+	char	*cmd;
+}	t_command;
+
+
+#endif
--- src/execution/execute.c
+++ src/execution/execute.c
@@ -6,7 +6,7 @@
 /*   By: ylagmah <ylagmah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 16:28:05 by ylagmah           #+#    #+#             */
-/*   Updated: 2025/02/21 15:36:39 by ylagmah          ###   ########.fr       */
+/*   Updated: 2025/02/23 08:55:17 by ylagmah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,39 +17,12 @@ void	ft_execute(t_shell *shell, t_execute *exe)
 	t_node	*node;
 	t_list	*lst;
 
-	// TODO
 	node = exe->node;
 	while (node)
 	{
 		if (ft_expand_node_vars(shell, node))
 			return ;
-		printf("%s\n", node->filter);
-		lst = ft_split(shell, node->content, node->filter);
-		while (lst)
-		{
-			printf("%s\n", (char *)lst->content);
-			lst = lst->next;
-		}
+		printf("%s\n", node->content);
 		node = node->next;
 	}
 }
-
-// printf("--> %s(%d) [input: ", node->content, node->type);
-// 		while (node->in)
-// 		{
-// 			printf("%s ", node->in->path);
-// 			node->in = node->in->next;
-// 		}
-// 		printf("]   [output: ");
-// 		while (node->out)
-// 		{
-// 			printf("%s ", node->out->path);
-// 			node->out = node->out->next;
-// 		}
-// 		printf("]   [here_doc: ");
-// 		while (node->here_doc)
-// 		{
-// 			printf("%s ", node->here_doc->path);
-// 			node->here_doc = node->here_doc->next;
-// 		}
-// 		printf("]\n");
