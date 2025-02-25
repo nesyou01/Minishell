0e4b1341d5d3d8cf5d9398a321255c6d64724af9
Abdelhak El Gadyry <ael-gady@e3r1p11.1337.ma>
2025-02-25 16:50:20 +0000

    with_ excution

--- src/minishell.c
+++ src/minishell.c
@@ -3,14 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ylagmah <ylagmah@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ael-gady <ael-gady@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 11:35:32 by ylagmah           #+#    #+#             */
-/*   Updated: 2025/02/23 08:57:31 by ylagmah          ###   ########.fr       */
+/*   Updated: 2025/02/25 14:12:09 by ael-gady         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+void	execute_tree(t_shell *shell, t_node *node)
+{
+	if (!node)
+		return ;
+	if (node->type == COMMAND)
+		execute_commande(shell, node);
+	else if (node->type == PIPE)
+		execute_pipe(shell , node);
+	else if (node->type == AND)
+		execute_and(shell, node);
+	else if (node->type == OR)
+		execute_or(shell, node);
+	else if (node->type == SUB_SHELL)
+		execute_subshell(shell, node);
+	// else if (node->type == OUT_REDIRECTER || node->type == IN_REDIRECTER ||
+	// 		 node->type == OUT_APPEND_REDIRECTER || node->type == HERE_DOC)
+	// 	execute_redirection(shell, node);
+}
 
 static void	minishell(t_shell *shell)
 {
@@ -25,7 +44,7 @@ static void	minishell(t_shell *shell)
 		ft_add_garbage(&(shell->cmd_garbage), shell, str);
 		node = ft_parser(shell, str);
 		if (node)
-			ft_execute(shell, node);
+			execute_tree(shell, node);//todo->
 		ft_clean_cmd(shell);
 	}
 	ft_clean_all(shell);
