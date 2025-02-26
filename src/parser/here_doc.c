1b417c3660c6a35517e7d5df0072ce1edc1acc50
Youness Lagmah <ylagmah@e2r9p15.1337.ma>
2025-02-26 16:54:55 +0000

    Do not expand if the limiter does not have '/"

--- src/parser/here_doc.c
+++ src/parser/here_doc.c
@@ -6,7 +6,7 @@ static char	*remove_quotes(t_shell *shell, t_token *token)
 
 	node = ft_new_node(shell, token);
 	ft_remove_quotes(shell, node);
-	return (node->content);
+	return (ft_expand_all_vars(shell, node->content));
 }
 
 static int	ft_pipe(int fds[2])
@@ -27,20 +27,22 @@ int	here_doc_handler(t_shell *shell, t_token *token)
 	char	*str;
 	char	*limiter;
 	int		fds[2];
+	int		has_quote;
 
 	if (ft_pipe(fds))
 		return (1);
+	has_quote = ft_strchr(token->content, '\'') || ft_strchr(token->content, '"');
 	token->prev->type = IN_REDIRECTER;
 	token->prev->fd = fds[0];
 	limiter = remove_quotes(shell, token);
-	limiter = ft_expand_all_vars(shell, limiter);
 	while (1)
 	{
 		str = readline("here_doc> ");
 		if (!str)
 			break ;
 		ft_add_cmd_garbage(shell, str);
-		str = ft_expand_all_vars(shell, str);
+		if (!has_quote)
+			str = ft_expand_all_vars(shell, str);
 		if (ft_strcmp(str, limiter) == 0)
 			break ;
 		ft_putstr_fd(str, fds[1]);
