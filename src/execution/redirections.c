84510ecd8d62b8fe8b36834b665f725086e959ce
Abdelhak El Gadyry <ael-gady@e3r1p5.1337.ma>
2025-02-27 17:11:28 +0000

    handle subshell, but current implementation is quite close to being complete

--- src/execution/redirections.c
+++ src/execution/redirections.c
@@ -6,7 +6,7 @@
 /*   By: ael-gady <ael-gady@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 09:56:46 by ael-gady          #+#    #+#             */
-/*   Updated: 2025/02/26 14:01:56 by ael-gady         ###   ########.fr       */
+/*   Updated: 2025/02/27 16:21:33 by ael-gady         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,36 +47,36 @@ void	handle_output_redirections(t_file *out)
 	close(fd);
 }
 
-void	handle_here_doc(t_file *heredoc)
-{
-	int		fd_tmp;
-	char	*line;
+// void	handle_here_doc(t_file *heredoc)
+// {
+// 	int		fd_tmp;
+// 	char	*line;
 
-	if (!heredoc || !heredoc->path)
-		return ;
-	fd_tmp = open("/tmp/.heredoc_tmp", O_RDWR | O_CREAT | O_TRUNC, 0644);
-	if (fd_tmp == -1)
-		ft_error("failed open heredoc_tmp");
-	heredoc->fd = open("/tmp/.heredoc_tmp", O_RDWR | O_CREAT | O_TRUNC, 0644);
-	if (heredoc->fd == -1)
-		ft_error("failed open heredoc_tmp");
-	// remove /tmp/.heredoc_tmp but we can work with him when my process in life!
-	unlink("/tmp/.heredoc_tmp");
-	while (1)
-	{
-		line = readline("heredoc> ");
-		if (!line)
-			break;
-		//here_doc->path in this case take the limiter in content !
-		if (ft_strcmp(line, heredoc->path)
-			&& line[ft_strlen(heredoc->path)] == '\n')
-		{
-			free(line);
-			break;
-		}
-		(write(fd_tmp, line, ft_strlen(line)), free(line));
-	}
-	close(fd_tmp);
-	if (dup2(heredoc->fd, STDIN_FILENO) == -1)
-		(close(heredoc->fd), ft_error("failed dup2 input !"));
-}
+// 	if (!heredoc || !heredoc->path)
+// 		return ;
+// 	fd_tmp = open("/tmp/.heredoc_tmp", O_RDWR | O_CREAT | O_TRUNC, 0644);
+// 	if (fd_tmp == -1)
+// 		ft_error("failed open heredoc_tmp");
+// 	heredoc->fd = open("/tmp/.heredoc_tmp", O_RDWR | O_CREAT | O_TRUNC, 0644);
+// 	if (heredoc->fd == -1)
+// 		ft_error("failed open heredoc_tmp");
+// 	// remove /tmp/.heredoc_tmp but we can work with him when my process in life!
+// 	unlink("/tmp/.heredoc_tmp");
+// 	while (1)
+// 	{
+// 		line = readline("heredoc> ");
+// 		if (!line)
+// 			break;
+// 		//here_doc->path in this case take the limiter in content !
+// 		if (ft_strcmp(line, heredoc->path)
+// 			&& line[ft_strlen(heredoc->path)] == '\n')
+// 		{
+// 			free(line);
+// 			break;
+// 		}
+// 		(write(fd_tmp, line, ft_strlen(line)), free(line));
+// 	}
+// 	close(fd_tmp);
+// 	if (dup2(heredoc->fd, STDIN_FILENO) == -1)
+// 		(close(heredoc->fd), ft_error("failed dup2 input !"));
+// }
