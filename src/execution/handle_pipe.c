779bebc8fd88ce77475fd6bf159f1536ab22a23b
gadyry <87965749+gadyry@users.noreply.github.com>
2025-02-27 17:11:28 +0000

    handle subshell, but current implementation is quite close to being complete

--- src/execution/handle_pipe.c
+++ src/execution/handle_pipe.c
@@ -6,7 +6,7 @@
 /*   By: ael-gady <ael-gady@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 11:19:27 by ael-gady          #+#    #+#             */
-/*   Updated: 2025/02/26 13:08:12 by ael-gady         ###   ########.fr       */
+/*   Updated: 2025/02/27 16:30:08 by ael-gady         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ static void	left_child(t_shell *shell, t_node *node, int *pipe_fd)
 		(close(pipe_fd[1]), ft_error("failed output dup2 !"));
 	close(pipe_fd[1]);
 	execute_tree(shell, node->l_node);
-	exit(EXIT_SUCCESS);//the child processes after calling execute_tree is a safety measure
+	//exit(EXIT_SUCCESS);//the child processes after calling execute_tree is a safety measure
+	exit(node->exit_status);
 }
 
 static void	right_child(t_shell *shell, t_node *node, int *pipe_fd)
@@ -29,7 +30,8 @@ static void	right_child(t_shell *shell, t_node *node, int *pipe_fd)
 		(close(pipe_fd[0]), ft_error("failed output dup2 !"));
 	close(pipe_fd[0]);
 	execute_tree(shell, node->r_node);
-	exit(EXIT_SUCCESS);
+	//exit(EXIT_SUCCESS);
+	exit(node->exit_status);
 }
 
 void	execute_pipe(t_shell *shell, t_node *node)
--- src/execution/handle_subshell.c
+++ src/execution/handle_subshell.c
@@ -1,33 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   logical_operator.c                                 :+:      :+:    :+:   */
+/*   handle_subshell.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ael-gady <ael-gady@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/26 14:16:33 by ael-gady          #+#    #+#             */
-/*   Updated: 2025/02/27 16:38:30 by ael-gady         ###   ########.fr       */
+/*   Created: 2025/02/27 15:04:14 by ael-gady          #+#    #+#             */
+/*   Updated: 2025/02/27 16:50:59 by ael-gady         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	execute_logical(t_shell *shell, t_node *node)
+void	execute_subshell(t_shell *shell, t_node *node)
 {
-	int	l_status;
+	pid_t	pid;
+	int		status;
 
 	if (!node || !node->l_node || !node->r_node)
 		return ;
-	execute_tree(shell, node->l_node);
-	l_status = node->exit_status;
-	if (node->type == AND)
+	pid = fork();
+	if (pid == -1)
+		ft_error("failed fork !");
+	if (pid == 0)
 	{
-		if (l_status == 0)
-			execute_tree(shell, node->r_node);
+		execute_tree(shell, node->l_node);
+		exit(node->exit_status);
 	}
-	else if (node->type == OR)
+	else
 	{
-		if (l_status != 0)
-			execute_tree(shell, node->r_node);
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+			node->exit_status = WEXITSTATUS(status);
+		else
+			node->exit_status = 1;
 	}
 }
