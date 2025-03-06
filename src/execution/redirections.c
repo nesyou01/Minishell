1834d01c25b152989064af50380e20ab75d95c37
Youness Lagmah <ylagmah@e2r7p11.1337.ma>
2025-03-06 00:20:16 +0000

    Fix here doc

--- src/execution/redirections.c
+++ src/execution/redirections.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirections.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ael-gady <ael-gady@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ylagmah <ylagmah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 09:56:46 by ael-gady          #+#    #+#             */
-/*   Updated: 2025/03/04 22:10:38 by ael-gady         ###   ########.fr       */
+/*   Updated: 2025/03/06 00:19:18 by ylagmah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ void	handle_input_redirections(t_file *in)
 
 	if (!in || !in->path)
 		return ;
-	fd = open(in->path, O_RDONLY);
+	if (in->fd != -1)
+		fd = in->fd;
+	else
+		fd = open(in->path, O_RDONLY);
 	if (fd == -1)
 		ft_error("open failed !");
 	if (dup2(fd, STDIN_FILENO) == -1)
@@ -31,7 +34,7 @@ void	handle_output_redirections(t_file *out)
 	int	fd;
 	int	flags;
 
-	if (!out || out->path)
+	if (!out || !out->path)
 		return ;
 	// i need maybe a flag to check the type of redirection!
 	if (out->type_out_redirect == OUT_APPEND_REDIRECTER)
@@ -46,37 +49,3 @@ void	handle_output_redirections(t_file *out)
 		(close(fd), ft_error("failed dup2 output !"));
 	close(fd);
 }
-
-// void	handle_here_doc(t_file *heredoc)
-// {
-// 	int		fd_tmp;
-// 	char	*line;
-
-// 	if (!heredoc || !heredoc->path)
-// 		return ;
-// 	fd_tmp = open("/tmp/.heredoc_tmp", O_RDWR | O_CREAT | O_TRUNC, 0644);
-// 	if (fd_tmp == -1)
-// 		ft_error("failed open heredoc_tmp");
-// 	heredoc->fd = open("/tmp/.heredoc_tmp", O_RDWR | O_CREAT | O_TRUNC, 0644);
-// 	if (heredoc->fd == -1)
-// 		ft_error("failed open heredoc_tmp");
-// 	// remove /tmp/.heredoc_tmp but we can work with him when my process in life!
-// 	unlink("/tmp/.heredoc_tmp");
-// 	while (1)
-// 	{
-// 		line = readline("heredoc> ");
-// 		if (!line)
-// 			break;
-// 		//here_doc->path in this case take the limiter in content !
-// 		if (ft_strcmp(line, heredoc->path)
-// 			&& line[ft_strlen(heredoc->path)] == '\n')
-// 		{
-// 			free(line);
-// 			break;
-// 		}
-// 		(write(fd_tmp, line, ft_strlen(line)), free(line));
-// 	}
-// 	close(fd_tmp);
-// 	if (dup2(heredoc->fd, STDIN_FILENO) == -1)
-// 		(close(heredoc->fd), ft_error("failed dup2 input !"));
-// }
