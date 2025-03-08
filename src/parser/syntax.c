7cdd103d67196888866e0f2cbc2cbaad2c54f07f
Youness Lagmah <ylagmah@e2r7p6.1337.ma>
2025-03-08 02:14:14 +0000

    Fix multiple prentheses start

--- src/parser/syntax.c
+++ src/parser/syntax.c
@@ -6,7 +6,7 @@
 /*   By: ylagmah <ylagmah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 14:32:13 by ylagmah           #+#    #+#             */
-/*   Updated: 2025/03/07 23:46:05 by ylagmah          ###   ########.fr       */
+/*   Updated: 2025/03/08 02:12:41 by ylagmah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,8 @@ static int	is_valid_parentheses(t_token *token)
 {
 	if (token->type == PARENTHESES_START)
 		return (token->next && (token->next->type == COMMAND
-			|| is_redirection(token->next) || token->next->type == HERE_DOC));
+			|| is_redirection(token->next) || token->next->type == HERE_DOC
+			|| token->next->type == PARENTHESES_START));
 	if (token->type == PARENTHESES_END)
 		return (!token->next || (token->next->type != PARENTHESES_START
 			&& token->next->type != COMMAND));
