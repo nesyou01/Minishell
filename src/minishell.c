50d2e2e97df39a966a2a83572756c81f447a4148
Youness Lagmah <ylagmah@e3r10p11.1337.ma>
2025-03-09 00:43:46 +0000

    Some changes

--- src/minishell.c
+++ src/minishell.c
@@ -49,7 +49,6 @@ int	main(int argc, char **argv, char **env)
 		return (ft_perror("usage => ./minishell"), 1);
 	env_init(&shell, env);
 	shell.exit = 0;
-	rl_catch_signals = 0;
 	signals_listener(&shell);
 	minishell(&shell);
 	return (0);
