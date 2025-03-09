2b01bcb53380b8412582e369b1a2d5b5bf49d56d
Youness Lagmah <ylagmah@e3r1p5.1337.ma>
2025-03-09 23:32:07 +0000

    ft_split

--- src/utils/ft_split.c
+++ src/utils/ft_split.c
@@ -1,77 +1,75 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_split.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ylagmah <ylagmah@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/13 16:27:45 by ylagmah           #+#    #+#             */
-/*   Updated: 2025/03/09 23:14:21 by ylagmah          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "../../includes/minishell.h"
 
-static int	ft_get_quote_end(char *str, char quote, int start)
+static size_t	split_size(char const *s, char c)
 {
-	int	i;
-	int	quotes;
+	size_t	i;
+	size_t	size;
 
-	quotes = 1;
-	i = start + quotes;
-	while (str[i])
-	{
-		if (str[i] == quote)
-			quotes++;
-		if (quotes % 2 == 0 && ft_strchr(SEPECIAL_CHARS, str[i]))
-			break ;
-		i++;
-	}
-	return (i);
-}
-
-static int	ft_get_token_end(char *str)
-{
-	int		i;
-	char	*first;
-
+	size = 1;
 	i = 0;
-	first = ft_strchr(SEPECIAL_CHARS, *str);
-	while (str[i] && str[i] != ' '
-		&& (!ft_strchr(SEPECIAL_CHARS, str[i]) == !first))
-	{
-		if (str[i] == '(' || str[i] == ')')
-			return (1);
-		if (str[i] == '\'')
-			return (ft_get_quote_end(str, '\'', i));
-		if (str[i] == '"')
-			return (ft_get_quote_end(str, '"', i));
+	while (s[i] && s[i] == c)
 		i++;
-	}
-	return (i);
-}
-
-t_list	*ft_split_node(t_shell *shell, t_node *node)
-{
-	t_list		*lst;
-	int			i;
-	int			end;
-	char		*content;
-
-	i = 0;
-	lst = NULL;
-	if (!node->content)
-		return (NULL);
-	while (1)
+	if (!s[i])
+		return (0);
+	while (s[i])
 	{
-		if (node->filter[i] == ' ')
+		if (s[i] == c)
+		{
+			while (s[i] == c)
+				i++;
+			if (s[i])
+				size++;
+		}
+		else
 			i++;
-		end = ft_get_token_end(node->filter + i);
-		content = ft_substr(shell, node->content, i, end);
-		ft_lstadd_back(&lst, ft_lstnew(shell, content));
-		i += end;
-		if (!node->filter[i])
-			break ;
 	}
-	return (lst);
+	return (size);
+}
+
+static char	*split_to_c(t_shell *shell, char const *s, char c)
+{
+	size_t	ri;
+	char	*result;
+	size_t	len;
+
+	len = 0;
+	while (s[len] && s[len] != c)
+		len++;
+	ri = 0;
+	result = (char *) ft_malloc (shell, (len + 1) * sizeof(char));
+	if (!result)
+		return (NULL);
+	while (*s && *s != c)
+	{
+		result[ri] = *s;
+		ri++;
+		s++;
+	}
+	result[ri] = '\0';
+	return (result);
+}
+
+char	**ft_split(t_shell *shell, char const *s, char c)
+{
+	char	**result;
+	size_t	ri;
+	size_t	size;
+
+	if (!s)
+		return (NULL);
+	ri = 0;
+	size = split_size(s, c);
+	result = (char **) ft_malloc(shell, sizeof(char *) * (size + 1));
+	if (!result)
+		return (NULL);
+	while (ri++ < size)
+	{
+		while (*s && *s == c)
+			s++;
+		result[ri - 1] = split_to_c(shell, s, c);
+		while (*s && *s != c)
+			s++;
+	}
+	result[ri - 1] = NULL;
+	return (result);
 }
