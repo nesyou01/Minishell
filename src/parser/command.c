ca389ab302969abe6c5735056f746dc5e5fc7b37
Abdelhak El Gadyry <ael-gady@e3r10p12.1337.ma>
2025-03-09 00:41:53 +0000

    fix cmd

--- src/parser/command.c
+++ src/parser/command.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   command.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ylagmah <ylagmah@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ael-gady <ael-gady@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 13:02:38 by ylagmah           #+#    #+#             */
-/*   Updated: 2025/03/05 21:46:54 by ylagmah          ###   ########.fr       */
+/*   Updated: 2025/03/08 23:48:22 by ael-gady         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ t_command	*ft_parse_command(t_shell *shell, t_node *node)
 
 	if (node->type != COMMAND)
 		return (NULL);
-	result->envp = ft_get_all_env(shell);
 	argv = ft_lst_to_array(shell, ft_split_node(shell, node));
 	result = ft_malloc(shell, sizeof(t_command));
+	result->envp = ft_get_all_env(shell);
 	result->argv = argv;
 	result->cmd = argv[0];
 	return (result);
