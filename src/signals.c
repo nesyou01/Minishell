c0bcd41e3e4e284ebae6b014ac66448257c1cf07
Youness Lagmah <ylagmah@e3r10p11.1337.ma>
2025-03-09 00:37:49 +0000

    ctrl-\ does nothing.

--- src/signals.c
+++ src/signals.c
@@ -1,14 +1,17 @@
 #include "../includes/minishell.h"
 
 static void	on_new_prompt(int signal) {
+	(void) signal;
 	printf("\n");
 	rl_on_new_line();
-    rl_replace_line("", 0);
-    rl_redisplay();
+	rl_replace_line("", 0);
+	rl_redisplay();
 }
+
 
 void	signals_listener(t_shell *shell)
 {
-	if (signal(SIGINT, on_new_prompt) == SIG_ERR)
+	if (signal(SIGINT, on_new_prompt) == SIG_ERR
+		|| signal(SIGQUIT, SIG_IGN) == SIG_ERR)
 		ft_error("signal failed");
 }
