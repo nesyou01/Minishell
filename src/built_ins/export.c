3e12cccacc72e392dd09f1f1e85bab86964c715b
Youness Lagmah <ylagmah@e3r6p1.1337.ma>
2025-03-10 23:56:09 +0000

    export mutiple vars

--- src/built_ins/export.c
+++ src/built_ins/export.c
@@ -6,7 +6,7 @@
 /*   By: ylagmah <ylagmah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 11:34:38 by ylagmah           #+#    #+#             */
-/*   Updated: 2025/03/10 23:34:38 by ylagmah          ###   ########.fr       */
+/*   Updated: 2025/03/10 23:55:13 by ylagmah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,13 +38,11 @@ static int	is_concat(char *key_value)
 	return (0);
 }
 
-static void	set_env(t_shell *shell, char *key, char *key_value)
+static void	set_env(t_shell *shell, t_env *env, char *key_value, char *key)
 {
-	t_env	*env;
-	char	*value;
 	t_env	*last;
+	char	*value;
 
-	env = ft_get_env(shell->env, key);
 	if (!env)
 	{
 		last = ft_last_env(shell->env);
@@ -63,21 +61,36 @@ static void	set_env(t_shell *shell, char *key, char *key_value)
 	}
 }
 
-int	ft_export(t_shell *shell, t_command *cmd)
+static int	handle_env(t_shell *shell, char *key_value)
 {
+	t_env	*env;
 	char	*key;
-	char	*key_value;
 
-	key_value = cmd->argv[1];
-	if (!key_value)
-		return (print_env(shell), 1);
 	if (*key_value == '-' && key_value[1])
 		return (ft_perror2("invalid options", key_value), 1);
 	key = ft_get_env_key(shell, key_value);
 	if (!ft_is_valid_key(key))
 		return (ft_perror2("invalid identifier", key), 1);
-	set_env(shell, key, key_value);
-	if (key_value && cmd->argv[2])
-		return (ft_perror2("invalid identifier", cmd->argv[2]), 1);
+	env = ft_get_env(shell->env, key);
+	set_env(shell, env, key_value, key);
 	return (0);
+}
+
+int	ft_export(t_shell *shell, t_command *cmd)
+{
+	int		is_fail;
+	int		i;
+
+	i = 1;
+	if (!cmd->argv[i])
+		return (print_env(shell), 1);
+	is_fail = 0;
+	while (cmd->argv[i])
+	{
+		if (!is_fail)
+			is_fail = handle_env(shell, cmd->argv[i++]);
+		else
+			handle_env(shell, cmd->argv[i++]);
+	}
+	return (is_fail);
 }
