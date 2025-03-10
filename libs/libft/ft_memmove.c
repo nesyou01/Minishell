549cd9e67847279ca6f46f966836ceffacf788db
Youness Lagmah <ylagmah@e2r9p5.1337.ma>
2025-03-10 04:36:52 +0000

    ambiguous redirect

--- libs/libft/ft_memmove.c
+++ libs/libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ylagmah <ylagmah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 13:35:39 by ylagmah           #+#    #+#             */
-/*   Updated: 2024/11/12 20:43:35 by ylagmah          ###   ########.fr       */
+/*   Updated: 2025/03/10 03:06:45 by ylagmah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,11 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	char	*s;
 	char	*d;
-	size_t	i;
 
 	if (!dst && !src)
 		return (NULL);
 	s = (char *)src;
 	d = (char *)dst;
-	i = 0;
 	if (d > s)
 	{
 		while (len-- > 0)
