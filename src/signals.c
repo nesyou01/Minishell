47ab9a9f959f255bab961b90e5a709a7978369a5
Youness Lagmah <ylagmah@e3r9p2.1337.ma>
2025-03-12 00:11:10 +0000

    Fix signals

--- src/signals.c
+++ src/signals.c
@@ -9,11 +9,20 @@ static void	on_new_prompt(int signal) {
 	rl_redisplay();
 }
 
+static void	on_exit(int signal) {
+	(void) signal;
+
+	printf("\n");
+	exit(99);
+}
+
 void	signals_listener(int action)
 {
 	signal(SIGQUIT, SIG_IGN);
 	if (action == 0)
 		signal(SIGINT, on_new_prompt);
 	else if (action == 1)
-		signal(SIGINT, SIG_DFL);
+		signal(SIGINT, on_exit);
+	else if (action == 2)
+		signal(SIGINT, SIG_IGN);
 }
