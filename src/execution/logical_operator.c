f852c7efbf968199d28e19d0d57ba646d2b6a181
Abdelhak El Gadyry <ael-gady@e3r10p18.1337.ma>
2025-04-12 18:52:40 +0100

    issam grissag deen

--- src/execution/logical_operator.c
+++ src/execution/logical_operator.c
@@ -6,84 +6,84 @@
 /*   By: ael-gady <ael-gady@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 14:16:33 by ael-gady          #+#    #+#             */
-/*   Updated: 2025/04/12 15:57:09 by ael-gady         ###   ########.fr       */
+/*   Updated: 2025/04/12 17:35:52 by ael-gady         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	execute_and(t_shell *shell, t_node *node)
-{
-	int	r_status;
-
-	if (node->l_node->exit_status == 0)
-	{
-		execute_tree(shell, node->r_node);
-		r_status = node->r_node->exit_status;
-		node->exit_status = r_status;
-	}
-	else
-		node->exit_status = node->l_node->exit_status;
-}
-
-void	execute_or(t_shell *shell, t_node *node)
-{
-	int	r_status;
-
-	if (node->l_node->exit_status != 0)
-	{
-		execute_tree(shell, node->r_node);
-		r_status = node->r_node->exit_status;
-		node->exit_status = r_status;
-	}
-	else
-		node->exit_status = node->l_node->exit_status;
-}
-
-void	execute_logical(t_shell *shell, t_node *node)
-{
-	if (!node || !node->l_node || !node->r_node)
-		return ;
-
-	execute_tree(shell, node->l_node);
-
-	if (node->type == AND)
-		execute_and(shell, node);
-	else if (node->type == OR)
-		execute_or(shell, node);
-}
-
 // void	execute_and(t_shell *shell, t_node *node)
 // {
 // 	int	r_status;
 
-// 	if (exit_status(0, 0) == 0)
+// 	if (node->l_node->exit_status == 0)
 // 	{
 // 		execute_tree(shell, node->r_node);
-// 		r_status = exit_status(0, 0);+
-// 		exit_status(1, r_status);
+// 		r_status = node->r_node->exit_status;
+// 		node->exit_status = r_status;
 // 	}
+// 	else
+// 		node->exit_status = node->l_node->exit_status;
 // }
 
 // void	execute_or(t_shell *shell, t_node *node)
 // {
 // 	int	r_status;
 
-// 	if (exit_status(0, 0) != 0)
+// 	if (node->l_node->exit_status != 0)
 // 	{
 // 		execute_tree(shell, node->r_node);
-// 		r_status = exit_status(0, 0);
-// 		exit_status(1, r_status);
+// 		r_status = node->r_node->exit_status;
+// 		node->exit_status = r_status;
 // 	}
+// 	else
+// 		node->exit_status = node->l_node->exit_status;
 // }
 
 // void	execute_logical(t_shell *shell, t_node *node)
 // {
 // 	if (!node || !node->l_node || !node->r_node)
 // 		return ;
+
 // 	execute_tree(shell, node->l_node);
+
 // 	if (node->type == AND)
 // 		execute_and(shell, node);
 // 	else if (node->type == OR)
 // 		execute_or(shell, node);
 // }
+
+void	execute_and(t_shell *shell, t_node *node)
+{
+	int	r_status;
+
+	if (exit_status(0, 0) == 0)
+	{
+		execute_tree(shell, node->r_node);
+		r_status = exit_status(0, 0);
+		exit_status(1, r_status);
+	}
+}
+
+void	execute_or(t_shell *shell, t_node *node)
+{
+	int	r_status;
+
+	if (exit_status(0, 0) != 0)
+	{
+		execute_tree(shell, node->r_node);
+		r_status = exit_status(0, 0);
+		exit_status(1, r_status);
+	}
+}
+
+void	execute_logical(t_shell *shell, t_node *node)
+{
+	if (!node || !node->l_node || !node->r_node)
+		return ;
+	execute_tree(shell, node->l_node);
+	if (node->type == AND)
+		execute_and(shell, node);
+	else if (node->type == OR)
+		execute_or(shell, node);
+}
