b6d87c8f23cdd8818a9687235cf63d1744951959
Abdelhak El Gadyry <ael-gady@e3r1p5.1337.ma>
2025-04-13 09:25:29 +0100

    exit

--- includes/minishell.h
+++ includes/minishell.h
@@ -122,6 +122,8 @@ int			ft_unset(t_shell *shell, t_command *cmd);
 int			ft_pwd(t_shell *shell, t_command *parse_cmd);
 int			ft_cd(t_shell *shell, t_command *parse_cmd);
 int			ft_env(t_shell *shell, t_command *cmd);
-int			ft_builtin_exit(t_shell *shell, t_command *parse_cmd);
+int			ft_builtin_exit(t_shell *shell, t_node *node, t_command *parse_cmd);
+
+int			exit_status(int action, int value);
 
 #endif
