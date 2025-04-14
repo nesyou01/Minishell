b912e5d9439118064a39679b70e43197cb77d1d8
Youness Lagmah <lagmahraja@gmail.com>
2025-04-14 16:21:19 +0100

    Store exist status

--- src/execution/execute_cmd.c
+++ src/execution/execute_cmd.c
@@ -22,24 +22,26 @@ int	is_builtin(t_command *p_cmd)
 void	execute_builtin(t_shell *shell, t_node *node, t_command *p_cmd)
 {
 	int	fd;
+	int	status;
 
 	fd = dup(1);
 	if (!handle_redirections(node->io))
 		return ;
 	if (!ft_strcmp2(p_cmd->argv[0], "cd"))
-		ft_cd(shell, p_cmd);
+		status = ft_cd(shell, p_cmd);
 	else if (!ft_strcmp2(p_cmd->argv[0], "echo"))
-		ft_echo(p_cmd);
+		status = ft_echo(p_cmd);
 	else if (!ft_strcmp2(p_cmd->argv[0], "export"))
-		ft_export(shell, p_cmd);
+		status = ft_export(shell, p_cmd);
 	else if (!ft_strcmp2(p_cmd->argv[0], "unset"))
-		ft_unset(shell, p_cmd);
+		status = ft_unset(shell, p_cmd);
 	else if (!ft_strcmp2(p_cmd->argv[0], "env"))
-		ft_env(shell, p_cmd);
+		status = ft_env(shell, p_cmd);
 	else if (!ft_strcmp2(p_cmd->argv[0], "pwd"))
-		ft_pwd(shell, p_cmd);
+		status = ft_pwd(shell, p_cmd);
 	else if (!ft_strcmp2(p_cmd->argv[0], "exit"))
-		ft_builtin_exit(shell, node, p_cmd);
+		status = ft_builtin_exit(shell, node, p_cmd);
+	exit_status(1, status);
 	if (dup2(fd, 1) == -1)
 		ft_error(shell, node, "failed dup", 255);
 	close(fd);
