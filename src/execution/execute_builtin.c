3e7a3a68c21d62ee8ee41e81d8bf36303478d9a9
gadyry <87965749+gadyry@users.noreply.github.com>
2025-04-16 23:04:11 +0100

    fix the problem of redirect input  with saving tty and reset it after executing builtin

--- src/execution/execute_builtin.c
+++ src/execution/execute_builtin.c
@@ -6,7 +6,7 @@
 /*   By: ael-gady <ael-gady@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 13:51:51 by ylagmah           #+#    #+#             */
-/*   Updated: 2025/04/16 20:36:26 by ael-gady         ###   ########.fr       */
+/*   Updated: 2025/04/16 23:01:41 by ael-gady         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,50 @@ static void	set_error(void)
 	exit_status(1, 1);
 }
 
+static void	save_tty(t_shell *shell, t_node *node, int *fds)
+{
+	fds[0] = dup(0);
+	if (fds[0] == -1)
+		ft_error(shell, node, "failed dup", 1);
+	fds[1] = dup(1);
+	if (fds[1] == -1)
+	{
+		close(fds[0]);
+		ft_error(shell, node, "failed dup", 1);
+	}
+	fds[2] = dup(2);
+	if (fds[1] == -1)
+	{
+		(close(fds[0]), close(fds[1]));
+		ft_error(shell, node, "failed dup", 1);
+	}
+}
+
+static void	reset_tty(t_shell *shell, t_node *node, int *fd)
+{
+	if (dup2(fd[0], 0) == -1)
+	{
+		(close(fd[0]), close(fd[1]), close(fd[2]));
+		ft_error(shell, node, "failed dup", 1);
+	}
+	if (dup2(fd[1], 1) == -1)
+	{
+		(close(fd[0]), close(fd[1]), close(fd[2]));
+		ft_error(shell, node, "failed dup", 1);
+	}
+	if (dup2(fd[2], 2) == -1)
+	{
+		(close(fd[0]), close(fd[1]), close(fd[2]));
+		ft_error(shell, node, "failed dup", 1);
+	}
+}
+
 void	execute_builtin(t_shell *shell, t_node *node, t_command *p_cmd)
 {
-	int	fd;
+	int	fds[3];
 	int	status;
 
-	fd = dup(1);
-	if (fd == -1)
-		ft_error(shell, node, "failed dup", 1);
+	save_tty(shell, node, fds);
 	if (!handle_redirections(node->io))
 		return (set_error());
 	if (!ft_strcmp2(p_cmd->argv[0], "cd"))
@@ -55,7 +91,7 @@ void	execute_builtin(t_shell *shell, t_node *node, t_command *p_cmd)
 		status = ft_pwd(shell, p_cmd);
 	else
 		status = ft_builtin_exit(shell, node, p_cmd);
-	if (dup2(fd, 1) == -1)
-		ft_error(shell, node, "failed dup", 1);
-	(exit_status(1, status), close(fd));
+	reset_tty(shell, node, fds);
+	exit_status(1, status);
+	(close(fds[0]), close(fds[1]), close(fds[2]));
 }
