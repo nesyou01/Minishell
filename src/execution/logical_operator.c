89787157d28bd24a1512d2a401b19a82bf21357c
Youness Lagmah <lagmahraja@gmail.com>
2025-04-18 11:07:04 +0100

    Clean

--- src/execution/logical_operator.c
+++ src/execution/logical_operator.c
@@ -3,55 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   logical_operator.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ael-gady <ael-gady@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ylagmah <ylagmah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 14:16:33 by ael-gady          #+#    #+#             */
-/*   Updated: 2025/04/13 17:24:02 by ael-gady         ###   ########.fr       */
+/*   Updated: 2025/04/18 11:06:52 by ylagmah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-// void	execute_and(t_shell *shell, t_node *node)
-// {
-// 	int	r_status;
-
-// 	if (node->l_node->exit_status == 0)
-// 	{
-// 		execute_tree(shell, node->r_node);
-// 		r_status = node->r_node->exit_status;
-// 		node->exit_status = r_status;
-// 	}
-// 	else
-// 		node->exit_status = node->l_node->exit_status;
-// }
-
-// void	execute_or(t_shell *shell, t_node *node)
-// {
-// 	int	r_status;
-
-// 	if (node->l_node->exit_status != 0)
-// 	{
-// 		execute_tree(shell, node->r_node);
-// 		r_status = node->r_node->exit_status;
-// 		node->exit_status = r_status;
-// 	}
-// 	else
-// 		node->exit_status = node->l_node->exit_status;
-// }
-
-// void	execute_logical(t_shell *shell, t_node *node)
-// {
-// 	if (!node || !node->l_node || !node->r_node)
-// 		return ;
-
-// 	execute_tree(shell, node->l_node);
-
-// 	if (node->type == AND)
-// 		execute_and(shell, node);
-// 	else if (node->type == OR)
-// 		execute_or(shell, node);
-// }
 
 void	execute_and(t_shell *shell, t_node *node)
 {
